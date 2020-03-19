494eff68d2de6528f13ad1033860e9983b803825
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-19 13:30:29 +0100

    All client and server functionality implemented, ready for proper styling

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
@@ -11,9 +14,9 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -21,58 +24,50 @@ const Chat = ({ location }) => {
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name)
 
-    socket.emit('join', { name, room }, () => {
-
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
     });
-
-    return () => {
-      socket.emit('disconnect');
-      socket.off();
-    }
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
-    socket.on('message', message => {
-      setMessages([...messages, message])
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
     });
-  }, [messages]);
 
-  // helper function for sending messages
-  const sendMessage = event => {
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    })
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    }
+  }, [messages])
+
+  const sendMessage = (event) => {
     event.preventDefault();
 
-    socket.emit('sendMessage', message, () => {
-      setMessage('');
-    });
-  };
-
-  // Event handlers
-
-  const handleChange = event => {
-    setMessage(event.target.value);
-  };
-
-  const handleSendMsg = event => {
-    return event.key === 'Enter' ? sendMessage(event) : null;
-  };
-
-  console.log(message, messages);
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
 
   return (
-    <div className='outerContainer'>
-      <div className='container'>
+    <div className="outerContainer">
+      <div className="container">
         <InfoBar room={room} />
-        {/* <input
-          value={message}
-          onChange={handleChange}
-          onKeyPress={handleSendMsg}
-        /> */}
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users} />
     </div>
   );
-};
+}
 
 export default Chat;
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,32 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 import './Join.css';
 
-const Join = () => {
+export default function SignIn() {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
 
-  const handleChange = prop => event => {
-    prop === 'name' && setName(event.target.value);
-    prop === 'room' && setRoom(event.target.value);
-  };
-
-  const handleEmptyInput = event => {
-    return (!name || !room) ? event.preventDefault() : null;
-  };
-
   return (
-    <div className='joinOuterContainer'>
-      <div className='joinInnerContainer'>
-        <h1 className='heading'>Join</h1>
-        <div><input placeholder='Name' className='joinInput' type='text' onChange={handleChange('name')} /></div>
-        <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={handleChange('room')} /></div>
-        <Link onClick={handleEmptyInput} to={`/chat?name=${name}&room=${room}`}>
-          <button className='button mt-20' type='submit'>Sign in</button>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div>
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+        </div>
+        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className={'button mt-20'} type="submit">Sign In</button>
         </Link>
       </div>
     </div>
   );
-};
-
-export default Join;
+}
