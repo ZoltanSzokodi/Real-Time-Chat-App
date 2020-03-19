bb8c2ef3e1f92974ae6cfcc998e70899eff7cf39
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-19 10:53:21 +0100

    Established the socket client - server connection + helper functions for the user events

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Join.css';
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  const handleChange = prop => event => {
+    prop === 'name' && setName(event.target.value);
+    prop === 'room' && setRoom(event.target.value);
+  };
+
+  const handleEmptyInput = event => {
+    return (!name || !room) ? event.preventDefault() : null;
+  };
+
+  console.log('name ' + name);
+  console.log('room ' + room);
+
+  return (
+    <div className='joinOuterContainer'>
+      <div className='joinInnerContainer'>
+        <h1 className='heading'>Join</h1>
+        <div><input placeholder='Name' className='joinInput' type='text' onChange={handleChange('name')} /></div>
+        <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={handleChange('room')} /></div>
+        <Link onClick={handleEmptyInput} to={`/chat?name=${name}&room=${room}`}>
+          <button className='button mt-20' type='submit'>Sign in</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUserInRoom
+} = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +17,14 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', socket => {
-  console.log('We have a connection!');
+  socket.on('join', ({ name, room }, callback) => {
+    // addUser returns either an error msg or the user obj
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+  });
 
   socket.on('disconnect', () => {
     console.log('User has left')
